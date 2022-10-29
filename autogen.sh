ba8f41aea9f6903220da8efbb626404c6e63b67d
mcuee <xiaofanc@gmail.com>
2022-10-29 22:13:35 +0800

    Adopt libusb-1.0 autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,9 @@
 
 set -e
 
-./bootstrap.sh
-./configure --enable-examples-build $*
+srcdir="$(dirname "$0")"
+
+"$srcdir"/bootstrap.sh
+if [ -z "$NOCONFIGURE" ]; then
+    exec "$srcdir"/configure --enable-examples-build "$@"
+fi
